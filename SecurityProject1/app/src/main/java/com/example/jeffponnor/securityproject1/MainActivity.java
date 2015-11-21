0018ac40785fd779eebe87a41021ba949e544958
Jeff <jsp282@cornell.edu>
2015-11-21 16:45:25 -0500

    Added some fixes

--- SecurityProject1/app/src/main/java/com/example/jeffponnor/securityproject1/MainActivity.java
+++ SecurityProject1/app/src/main/java/com/example/jeffponnor/securityproject1/MainActivity.java
@@ -21,15 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static AudioManager AM;
     public static MediaPlayer MP;
-    private final int duration = 3; // seconds
-    private final int sampleRate = 8000;
-    private final int numSamples = duration * sampleRate;
-    private final double sample[] = new double[numSamples];
-    private final double freqOfTone = 440; // hz
 
-    private final byte generatedSnd[] = new byte[2 * numSamples];
-    Handler handler = new Handler();
-    private final int soundPlaying = 0;
 
 
 
